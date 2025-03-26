90a6820b669dc4b6c30cf8011139bd67be44402c
richard_adhika <radhik11@students.cs.ubc.ca>
2025-03-26 14:40:00 -0700

    add insert default values to the table

--- appService.js
+++ appService.js
@@ -75,10 +75,6 @@ async function testOracleConnection() {
     });
 }
 
-// =============================================================================
-// SAMPLE CODE ENDS HERE !
-// =====================================================================
-
 // =======================
 // ROUTE TABLE
 // =======================
@@ -87,6 +83,10 @@ async function initiateRouteTable() {
     return await withOracleDB(async (connection) => {
         try {
             await connection.execute(`DROP TABLE ORDERTABLE`);
+        } catch(err) {
+            console.log('Order table does not exist yet!');
+        }
+        try {
             await connection.execute(`DROP TABLE ROUTETABLE`);
         } catch(err) {
             console.log('Table might not exist, proceeding to create...');
@@ -95,11 +95,32 @@ async function initiateRouteTable() {
         const routeTableResult = await connection.execute(`
             CREATE TABLE ROUTETABLE (
                 routeId CHAR(6) PRIMARY KEY,
-                origin VARCHAR2(20) NOT NULL,
-                destination VARCHAR2(20) NOT NULL,
+                origin VARCHAR2(30) NOT NULL,
+                destination VARCHAR2(30) NOT NULL,
                 distance NUMBER
             )
         `);
+
+        await connection.execute(`
+            INSERT INTO ROUTETABLE (routeId, origin, destination, distance)
+            VALUES ('r00001', '49.25761407, -123.23615578', '49.27048682, -123.15760743', 10)`, 
+            [],
+            { autoCommit: true }
+        );
+
+        await connection.execute(`
+            INSERT INTO ROUTETABLE (routeId, origin, destination, distance)
+            VALUES ('r00002', '49.22764848, -123.06627330', '49.13373432, -122.83702854', 35)`, 
+            [],
+            { autoCommit: true }
+        );
+
+        await connection.execute(`
+            INSERT INTO ROUTETABLE (routeId, origin, destination, distance)
+            VALUES ('r00003', '43.69039231, -79.28855125', '43.65886249, -79.48819193', 22)`, 
+            [],
+            { autoCommit: true }
+        );
         return true;
     }).catch(() => {
         return false;
@@ -163,12 +184,33 @@ async function initiateOrderTable() {
                 weight NUMBER,
                 routeId CHAR(6) NOT NULL,
                 orderDate DATE,
-                departureTime CHAR(8),
-                arrivalTime CHAR(8),
+                departureTime CHAR(5),
+                arrivalTime CHAR(5),
                 FOREIGN KEY (routeId) REFERENCES ROUTETABLE
                     ON DELETE CASCADE
             )
         `);
+
+        await connection.execute(
+            `INSERT INTO ORDERTABLE (orderId, customerId, weight, routeId, orderDate, departureTime, arrivaltime)
+            VALUES ('o00001', 'c00001', 150, 'r00002', TO_DATE('2025-04-22', 'YYYY-MM-DD'), '06:00', '12:22')`,
+            [],
+            { autoCommit: true }
+        );
+
+        await connection.execute(
+            `INSERT INTO ORDERTABLE (orderId, customerId, weight, routeId, orderDate, departureTime, arrivaltime)
+            VALUES ('o00002', 'c00002', 150, 'r00001', TO_DATE('2025-03-29', 'YYYY-MM-DD'), '16:00', NULL)`,
+            [],
+            { autoCommit: true }
+        );
+
+        await connection.execute(
+            `INSERT INTO ORDERTABLE (orderId, customerId, weight, routeId, orderDate, departureTime, arrivaltime)
+            VALUES ('o00003', 'c00001', 150, 'r00001', TO_DATE('2025-04-01', 'YYYY-MM-DD'), '10:00', '22:00')`,
+            [],
+            { autoCommit: true }
+        );
         return true;
     }).catch(() => {
         return false;
--- init.sql
+++ init.sql
@@ -19,3 +19,21 @@ CREATE TABLE ORDERTABLE (
     FOREIGN KEY (routeId) REFERENCES ROUTETABLE
         ON DELETE CASCADE
 )
+
+INSERT INTO ROUTETABLE (routeId, origin, destination, distance)
+    VALUES ('r00001', '49.25761407, -123.23615578', '49.27048682, -123.15760743', 10)
+
+INSERT INTO ROUTETABLE (routeId, origin, destination, distance)
+    VALUES ('r00002', '49.22764848, -123.06627330', '49.13373432, -122.83702854', 35)
+
+INSERT INTO ROUTETABLE (routeId, origin, destination, distance)
+    VALUES ('r00003', '43.69039231, -79.28855125', '43.65886249, -79.48819193', 22)
+
+INSERT INTO ORDERTABLE (orderId, customerId, weight, routeId, orderDate, departureTime, arrivaltime)
+    VALUES ('o00001', 'c00001', 150, 'r00002', TO_DATE('2025-04-22', 'YYYY-MM-DD'), '06:00', '02:22')
+
+INSERT INTO ORDERTABLE (orderId, customerId, weight, routeId, orderDate, departureTime, arrivaltime)
+    VALUES ('o00002', 'c00002', 150, 'r00001', TO_DATE('2025-03-29', 'YYYY-MM-DD'), '16:00', NULL)
+
+INSERT INTO ORDERTABLE (orderId, customerId, weight, routeId, orderDate, departureTime, arrivaltime)
+    VALUES ('o00003', 'c00001', 150, 'r00001', TO_DATE('2025-04-01', 'YYYY-MM-DD'), '10:00', '22:00')
